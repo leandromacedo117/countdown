0632781d5d103335431ef230371ce3b814de8ed8
leandromacedo117 <146491504+leandromacedo117@users.noreply.github.com>
2023-11-09 10:19:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
 import Title from  './componentes/Title.jsx'
+import Counter from './componentes/Counter';
+import ImageFundo from './assets/fundo.jpg'
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(${ImageFundo})`}}>
       <div className="container">
         <Title title={"Contagem regressiva "}/>
         <div className="countdown-container">
-          <p>3 day</p>
+          <Counter title ={"Days"} number={2} ></Counter>
+          <Counter title ={"horaus"} number={2} ></Counter>
+          <Counter title ={"minuntes"} number={2} ></Counter>
+          <Counter title ={"segunds"} number={2} ></Counter>
         </div>
       </div>
     </div>
