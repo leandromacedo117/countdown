1fda29dd2b5f42bbfc097c1047fb489e19e16bd0
leandromacedo117 <146491504+leandromacedo117@users.noreply.github.com>
2023-11-15 09:25:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import ImageFundo from './assets/fundo.jpg'
+import ImageFundo from './assets/natal.jpg'
 import './App.css'
 import Title from  './componentes/Title.jsx'
 import Counter from './componentes/Counter';
@@ -9,17 +9,17 @@ import useCountdown from './hooks/useCountdown';
 
 
 function App() {
-  const x = useCountdown("Dec 25, 2023, 00:00:00")
+  const [day, hour, minute, segund] = useCountdown("Dec 25, 2023, 00:00:00")
   return (
     
     <div className="App" style={{backgroundImage: `url(${ImageFundo})` }}>
       <div className="container">
-        <Title title={"Contagem regressiva "}/>
+        <Title title={"Contagem regressiva para o Natal "}/>
         <div className="countdown-container">
-          <Counter title ={"Days"} number={2} ></Counter>
-          <Counter title ={"horaus"} number={2} ></Counter>
-          <Counter title ={"minuntes"} number={2} ></Counter>
-          <Counter title ={"segunds"} number={2} ></Counter>
+          <Counter title ={"Days"} number={day} ></Counter>
+          <Counter title ={"hours"} number={hour} ></Counter>
+          <Counter title ={"minutes"} number={minute} ></Counter>
+          <Counter title ={"segunds"} number={segund} ></Counter>
         </div>
       </div>
     </div>
